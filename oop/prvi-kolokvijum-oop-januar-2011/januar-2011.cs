dc27e83dfa808264e3c24a7c55b1477de2500fe4
sajtovi <cselic@ymail.com>
2016-01-31 09:48:20 -0800

    dodat uradjen zadatak za klasu Nekretnina

---
+++ oop/prvi-kolokvijum-oop-januar-2011/januar-2011.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: player
+ * Date: 1/17/2016
+ * Time: 10:32 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using System.Collections;
+using System.String;
+
+class Program
+{
+    static void Main()
+    {
+		string[] ime = {"Sanja", "Ana", "Nena", "Anita"};
+	
+	var rezultat = from n in ime
+		where n.StartsWith("A") select n;
+		
+		Console.WriteLine(rezultat);
+    }
+}
---
+++ oop/prvi-kolokvijum-oop-septembar-2015/hkjin.cs
@@ -0,0 +1,9 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: player
+ * Date: 1/25/2016
+ * Time: 12:30 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
