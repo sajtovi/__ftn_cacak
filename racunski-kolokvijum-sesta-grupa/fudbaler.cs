5068a9c2dcf8df3aa35a32fe64e19b07fe838f17
sajtovi <cselic@ymail.com>
2016-09-13 12:26:46 -0700

    Dodat zadatak sa klasom fudbaler

---
+++ racunski-kolokvijum-sesta-grupa/fudbaler.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+	public class Fudbaler
+	{
+		public int brojDresa;
+		public string imePrezime;
+		public int brojGolova;
+		public double plata;
+		
+		public Fudbaler()
+		{
+		}
+	}
+	
+	public void Sortirati(Fudbaler[] in
+}
